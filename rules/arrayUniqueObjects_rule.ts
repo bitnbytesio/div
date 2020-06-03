8932996fe42c3bba6fbf1a89cadb2624d78d1ee5
Dinesh <dineshkhurana333@gmail.com>
2020-06-03 15:32:38 +0530

    rule:arrayUniqueObjects

---
+++ rules/arrayUniqueObjects_rule.ts
@@ -0,0 +1,24 @@
+import { ValidationRuleContract } from "../contracts/validation_rule_contracts.ts";
+
+export function arrayUniqueObjects(value: any, args: Array<any>): ValidationRuleContract {
+  return {
+    name: "arrayUniqueObjects",
+    handler: (value: any) => {
+      if (!Array.isArray(value)) {
+        return false;
+      }
+
+      const result = new Set(value.map((o) => {
+        let output = '';
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const attr of args) {
+          output += o[attr];
+        }
+
+        return output;
+      }));
+      return result.size === value.length;
+    }
+  };
+}
