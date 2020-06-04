a59598345be6153027d34b1ea25c6ad1205cf2d6
Dinesh <dineshkhurana333@gmail.com>
2020-06-04 12:06:16 +0530

    rule:date_before

---
+++ rules/date_before_rule.ts
@@ -0,0 +1,25 @@
+import { ValidationRuleContract } from "../contracts/validation_rule_contracts.ts";
+import { moment } from "https://deno.land/x/moment/moment.ts";
+import { dateFormats } from "../util/date_util.ts";
+
+export function validateBefore(
+  args: Array<any>,
+): ValidationRuleContract {
+  return {
+    name: "validateBefore",
+    handler: (value: any) => {
+      const [beforeDate] = args;
+      const mBeforeDate = moment(beforeDate, dateFormats);
+      const mDate = moment(value, dateFormats);
+
+      if (
+        !mBeforeDate.isValid() || !mDate.isValid() ||
+        mBeforeDate.valueOf() < mDate.valueOf()
+      ) {
+        return false;
+      }
+
+      return true;
+    },
+  };
+}
