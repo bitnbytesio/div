055f3ba03f06c71dab4e1cdabb057289b2266e82
Harcharan Singh <bitnbytesio@gmail.com>
2020-06-04 12:13:53 +0530

    test case and fmt

--- rules_test/date_after_rule_test.ts
+++ rules_test/date_after_rule_test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "../test_deps.ts";
 import { dateAfter } from "../rules/date_after_rule.ts";
 
 Deno.test("rules:dateAfter", function (): void {
-  const ruleHandler = dateAfter([]).handler;
-  assertEquals(ruleHandler("20-12-2020"), true);
-  assertEquals(ruleHandler("20-12-2020"), false);
+  const ruleHandler = dateAfter(["20-05-2020"]).handler;
+  assertEquals(ruleHandler("20-06-2020"), true);
+  assertEquals(ruleHandler("20-04-2020"), false);
 });
