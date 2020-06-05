3c5d47ff64504281566fd1bb53017c9692a5d519
Dinesh <dineshkhurana333@gmail.com>
2020-06-05 11:49:16 +0530

    rule:numeric

---
+++ rules/digits_rule.ts
@@ -0,0 +1,11 @@
+import { validator } from "../deps.ts";
+import { ValidationRuleContract } from "../contracts/validation_rule_contracts.ts";
+
+export function numeric(): ValidationRuleContract {
+  return {
+    name: "numeric",
+    handler: (value: any) => {
+      //return validator.isNumeric(String(value));
+    },
+  };
+}
---
+++ rules_test/different_rule_test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from "../test_deps.ts";
+
+import { different } from "../rules/different_rule.ts";
+
+Deno.test("rules:different", function (): void {
+  const ruleHandler = different(["new_password"]).handler;
+  assertEquals(ruleHandler("000000", { new_password: "123456" }), true);
+  assertEquals(ruleHandler("000000", { new_password: "000000" }), true);
+});
