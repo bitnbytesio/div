9919b223aa285f80a5a9dd2ddadb22cd6787c5be
Dinesh <dineshkhurana333@gmail.com>
2020-06-08 15:35:08 +0530

    rule:sometimes

--- rules/sometimes_rule.ts
+++ rules/sometimes_rule.ts
@@ -3,9 +3,9 @@ import {
   ValidatorContract,
 } from "../contracts/validation_rule_contracts.ts";
 
-export function regex(args: Array<any>): ValidationRuleContract {
+export function sometimes(args: Array<any>): ValidationRuleContract {
   return {
-    name: "regex",
+    name: "sometimes",
     handler: (value: any, v: ValidatorContract) => {
       const [pattren] = args;
       const regexp = new RegExp(v.getAttributeValue(pattren));
